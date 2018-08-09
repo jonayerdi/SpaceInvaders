d147e8b718eadc860a1e86630698b1ab6946935f
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2018-08-09 17:35:14 +0200

    added assets loading w/ Promises

--- SpaceInvaders.js
+++ SpaceInvaders.js
@@ -1,15 +1,50 @@
 
+imageNames = [
+    "player",
+
+];
+imageURLs = imageNames.map(img => ("assets/" + img + ".png"));
+images = null;
+
+function LoadImagePromise(src) {
+    return new Promise((resolve, reject) => {
+        let img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = () => reject("Could not load " + src);
+        img.src = src;
+    })
+}
+
 class SpaceInvaders {
-    constructor(context, width=context.canvas.width, height=context.canvas.height, fps=30) {
+    constructor(context, fps=30) {
         this.context = context;
-        this.width = width;
-        this.height = height;
         this.fps = fps;
-        this.keydownFunction = (evt) => {this.onKeydown(evt)};
+        this.keydownFunction = (evt) => this.onKeydown(evt);
+        this.width = 400;
+        this.height = 400;
+        this.player = {x: 200, y: 10, lives: 3};
+    }
+    load() {
+        return new Promise((resolve, reject) => {
+            if(images) {
+                resolve();
+            } else {
+                Promise.all(imageURLs.map(img => LoadImagePromise(img))).then((imgArray) => {
+                    images = (() => {
+                        let result = new Map();
+                        imageNames.forEach((element, index) => {
+                            result.set(imageNames[index], imgArray[index]);
+                        });
+                        return result;
+                    })();
+                    resolve();
+                }).catch((reason) => reject(reason));
+            }
+        });
     }
     start() {
         document.addEventListener("keydown", this.keydownFunction);
-        this.intervalID = setInterval(() => {this.gameLoop()}, 1000/this.fps);
+        this.intervalID = setInterval(() => this.gameLoop(), 1000/this.fps);
     }
     stop() {
         clearInterval(this.intervalID);
